950b8d769b72b3afa223a844de897210407a9dbf
Ayesha Shabbir <ayeshashabbir@tenpearls.com>
2021-01-29 12:49:55 +0500

    First Commit

--- server.js
+++ server.js
@@ -23,7 +23,41 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+let responseObject = {}
 
+app.get('/api/timestamp/:input', (request, response) => {
+  let input = request.params.input;
+  
+  if(input.includes('-') || input.includes(' ')){
+    /* Date String */
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }else{
+    /* Timestamp */
+    input = parseInt(input)
+    
+    responseObject['unix'] = new Date(input).getTime()
+    responseObject['utc'] = new Date(input).toUTCString()
+  }
+  
+  if(!responseObject['unix'] || !responseObject['utc']){
+    response.json({error: 'Invalid Date'})
+  }
+  if(input === undefined){
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+  
+  }
+  
+  response.json(responseObject)
+})
+
+app.get('/api/timestamp', (request, response) => {
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+  
+  response.json(responseObject)
+})
 
 
 // listen for requests :)
